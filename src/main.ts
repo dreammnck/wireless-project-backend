9f2fba5a04725956de5bd09109e025cb851f0716
dreammnck <dreamnitchakran2543@gmail.com>
2022-10-06 11:59:02 +0700

    Prepare for deploying

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,64 @@
+import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
-  const host = configService.get<string>('host');
   const mqttUrl = configService.get<string>('mqtt.url');
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
+//   const projectId = "local-topic-364613";
+//   const region = "asia-east1";
+//   const registryId = "wireless-project-test1";
+//   const deviceId = "saline1";
+//   const serverCertFile = "/Volumes/project/wireless-project/project/backend/roots.pem"
+//   const privateKeyFile = "/Volumes/project/wireless-project/project/backend/rsa_private.pem"
+
+//   const mqttClientId = `projects/local-topic-364613/topics/saline`;
+//   const createJwt = (projectId: string, privateKeyFile: string) => {
+//   const token = {
+//     iat: Number(Date.now() / 1000),
+//     exp: Number(Date.now() / 1000) + 20 * 60, // 20 minutes
+//     aud: projectId,
+//   };
+//   const privateKey = readFileSync(privateKeyFile);
+//   const jwt = new JwtService({signOptions: {algorithm: "RS256"}});
+//   return jwt.sign(token, {privateKey, });
+// };
+
+//   const microserviceMqtt = app.connectMicroservice<MicroserviceOptions>({
+//     transport: Transport.MQTT,
+//     options: {
+//     host: "mqtt.googleapis.com",
+//     port: 8883,
+//     clientId: mqttClientId,
+//     username: 'unused',
+//     password:  createJwt(projectId, privateKeyFile ),
+//     protocol: 'mqtts',
+//     ca: [readFileSync(serverCertFile)],
+    
+//     },
+//   });
+
+
   const microserviceMqtt = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.MQTT,
     options: {
-      url: mqttUrl,
+      url: mqttUrl
     },
   });
 
   await app.startAllMicroservices();
-  await app.listen(port, host);
+  await app.listen(port);
 
   app.getUrl().then((url) => {
     console.log(`Application is running on ${url}`);
